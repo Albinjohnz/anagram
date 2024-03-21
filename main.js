e13520818128909346ca8be04f5bad26566b3125
Albinjohnz <albinjohnson77@gmail.com>
2024-03-21 10:10:04 +0530

    completed

--- main.js
+++ main.js
@@ -1,44 +1,9 @@
-const readline = require('readline');
+const prompt = require('prompt-sync')();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const s = prompt('Enter string s: ');
+const t = prompt('Enter string t: ');
 
-function isAnagram(s, t) {
-    // If the lengths of the strings are not the same, they cannot be anagrams
-    if (s.length !== t.length) {
-        return false;
-    }
+const sortedS = s.split('').sort().join('');
+const sortedT = t.split('').sort().join('');
 
-    // Create frequency maps for both strings
-    const frequencyS = {};
-    const frequencyT = {};
-
-    // Populate frequency map for string s
-    for (let char of s) {
-        frequencyS[char] = (frequencyS[char] || 0) + 1;
-    }
-
-    // Populate frequency map for string t
-    for (let char of t) {
-        frequencyT[char] = (frequencyT[char] || 0) + 1;
-    }
-
-    // Compare frequency maps
-    for (let char in frequencyS) {
-        if (frequencyS[char] !== frequencyT[char]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-rl.question('Enter string s: ', (s) => {
-    rl.question('Enter string t: ', (t) => {
-        const result = isAnagram(s, t);
-        console.log(`"${s}" and "${t}" are anagrams: ${result}`);
-        rl.close();
-    });
-});
+console.log(`"${s}" and "${t}" are ${sortedS === sortedT ? 'anagrams' : 'not anagrams'}.`);
